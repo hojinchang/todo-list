a91fc84fe8a8cc82cd390969420eadc50e03aa46
hojinchang <hojinchang1998@gmail.com>
2023-10-11 12:34:48 -0700

    Remove bug

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,6 @@ module.exports = {
        test: /\.(png|svg|jpg|jpeg|gif)$/i,
        type: 'asset/resource',
      },
-     {
-      test: /\.html$/,
-      use: 'html-loader',
-    },
     ],
   },
   plugins: [
