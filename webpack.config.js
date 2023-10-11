0cb51344582a1fbad015f5a631228677e489cb39
hojinchang <hojinchang1998@gmail.com>
2023-10-10 22:46:05 -0700

    Remove HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Todo List',
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
