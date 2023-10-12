bf868d74b91ea7a723ab10fdc258e559b258b324
hojinchang <hojinchang1998@gmail.com>
2023-10-12 01:07:32 -0700

    Remove empty lines

--- src/projects.js
+++ src/projects.js
@@ -1,15 +1,12 @@
 import { createSidebarProject } from "./createDom";
 
 const projects = (() => {
-
     const projects = [];
 
     const addProject = (projectName) => {
         const project = createSidebarProject(projectName);
         projects.push(project);
 
-        console.log(`Created a new project: ${projectName}`);
-        console.log(projects)
         return project;
     }
 
